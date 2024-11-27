b8d8d2c41d75341d3189c66c57b3d6e60d6a6e05
Alvaro Giovanni Castillo Muñoz <alvaroc.giovannim@gmail.com>
2024-11-27 16:40:48 -0300

    FEAT: Agregados modelos de usuario inicial

--- eGesVen/main/models.py
+++ eGesVen/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class TipoUsuario(models.Model):
+    id_tipo_usuario = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descripcion
+    
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    id_tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
